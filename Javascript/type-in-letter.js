1203d5f8d85c5b8b4a1b75f9438f8609c0fab834
Linda Westerlund <lindawesterlund94@gmail.com>
2023-11-27 11:57:42 +0100

    börjat på poängsystem och hints

--- Javascript/type-in-letter.js
+++ Javascript/type-in-letter.js
@@ -27,7 +27,13 @@ inputElement.addEventListener('input', function () {
     inputValue = inputElement.value.toUpperCase();
     inputElement.value = inputValue;
 });
-
+let allaBokstäver = []
+let correct 
+let points = 0
+let hint = -20
+spelOrd.split("").forEach((bokstavlista) => {
+	allaBokstäver.push(bokstavlista)
+})
 
 document.getElementById('letter-input').addEventListener('keydown', function (e)  {
 	if (e.key === 'Enter') {
@@ -49,15 +55,53 @@ document.getElementById('letter-input').addEventListener('keydown', function (e)
     // Lägg till rätt gissade bokstäver i listan
     if (spelOrd.includes(inputValue)) {
         listaBokstav.push(inputValue);
+		correct = true
     }
     else {
+		correct = false
         // Lägg till felgissade bokstäver i en annan sektion för felgissningar
         // Exempel: felGissningarSection.appendChild(document.createTextNode(inputValue));
     }
+	if(correct){
+		points += 29
+		correct = 0
 	}
+	else{
+		points -= 12
+		correct = 0
+	}
+	console.log("din poäng är: ", points, correct);
+}
     // Återställ input
     inputElement.value = "";
 
     console.log(`Det rätta ordet är ${spelOrd}`);
 });
 
+
+// hint-system
+let parent = document.querySelector(".type-in-letter")
+let hintIcon = document.createElement("p")
+hintIcon.innerText = "HINT ( -20p)"
+hintIcon.classList.add("hint")
+parent.appendChild(hintIcon)
+let hintBokstav 
+hintIcon.addEventListener("click", () =>{
+	console.log("cklickat!!!!")
+	points -= 20
+	function slumpabokstav() {
+		
+		hintBokstav = allaBokstäver[Math.floor(Math.random() * allaBokstäver.length)];
+		
+			
+		hintBokstav = hintBokstav.toUpperCase();
+		return hintBokstav;
+		
+	}
+	let insertBokstav = slumpabokstav()
+	let visadeBokstäver = []
+	visadeBokstäver.push[hintBokstav]
+	
+	let hurmånga = allaBokstäver.filter(x => x === insertBokstav)
+	console.log(hurmånga)
+})
